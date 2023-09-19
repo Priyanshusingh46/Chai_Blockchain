24eb27eefe70df3c98fb90510a7e02460496458a
Priyanshu46 <priyanshusingh1877@gmail.com>
2023-09-19 19:58:39 +0530

    first commit

---
+++ client/src/components/Memos.jsx
@@ -0,0 +1,92 @@
+import { useState,useEffect } from "react";
+import "./Memos.css"
+const Memos=({state})=>{
+    const [memos,setMemos]=useState([]);
+    const {contract}=state;
+    useEffect(()=>{
+        const memosMessage = async()=>{
+          console.log("1st");
+          console.log("okkk", contract);
+          try {
+            console.log("memos11111:");
+            const memos1 = await contract.getMemos();
+            // Handle the result from the getmomos() function
+            console.log("memos1:", memos1);
+            setMemos(memos1)
+          } catch (error) {
+            // Handle the error if the Promise is rejected
+            console.error("An error occurred:", error);
+          }
+          console.log("2nd");
+          
+          console.log("memos",memos)
+        }
+        contract && memosMessage()
+    },[contract])
+    return (
+        <div className="container-fluid">
+          <h3 style={{ textAlign: "center", marginTop: "20px" }}>Messages</h3>           
+                <table>
+                <tbody >
+          {memos.map((memo) => {
+            return (
+                    <tr >
+                      <td 
+                        style={{
+                          backgroundColor: "dodgerblue",
+                          border: "1px solid white",
+                          borderCollapse: "collapse",
+                          padding: "7px",
+                          width: "100px",
+                          color:"white",
+                         
+                        }}
+                      >
+                        {memo.name}
+                      </td>
+                      <td 
+                        style={{
+                          backgroundColor: "dodgerblue",
+                          border: "1px solid white",
+                          borderCollapse: "collapse",
+                          padding: "7px",
+                          width: "800px",
+                          color:"white"
+                        }}
+                      >
+                        {new Date(memo.timestamp * 1000).toLocaleString()}
+                      </td>
+                      <td  
+                        style={{
+                          backgroundColor: "dodgerblue",
+                          border: "1px solid white",
+                          borderCollapse: "collapse",
+                          padding: "7px",
+                          width: "300px",
+                          color:"white"
+                        }}
+                      >
+                        {memo.message}
+                      </td>
+                      <td  className="container-fluid"
+                        style={{
+                          backgroundColor: "dodgerblue",
+                          border: "1px solid white",
+                          borderCollapse: "collapse",
+                          padding: "7px",
+                          width: "400px",
+                          color:"white"
+                        }}
+                      >
+                        {memo.from}
+                      </td>
+                    </tr>
+             
+            );
+          })}
+               </tbody>
+                </table>
+        </div>
+      );
+}
+export default Memos;
---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Chai = await hre.ethers.getContractFactory("chai"); //fetching bytecode and ABI
+  const chai = await Chai.deploy(); //creating an instance of our smart contract
+
+  await chai.waitForDeployment();//deploying your smart contract
+
+  console.log("Deployed contract address:",chai.runner.address);
+}
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+//0x39F44A15615436BB9DE85A5F51278843CE1C75FA
